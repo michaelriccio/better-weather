45cd8491b48a7a4bbcd862479bfec369630ff16e
Michael Riccio <fchs219376@gmail.com>
2020-12-10 00:32:10 -0500

    Refactoring, aesthetic improvements

--- public/js/app.js
+++ public/js/app.js
@@ -19,62 +19,58 @@ const ctxHourlyTemp = document.getElementById('myChartHourlyTemp');
 const ctxHourlyPerc = document.getElementById('myChartHourlyPerc');
 const ctxDailyTemp = document.getElementById('myChartDailyTemp');
 const ctxDailyPerc = document.getElementById('myChartDailyPerc');
-let data1;
 let hourlyTemp = [];
 let hourlyPerc = [];
 let dailyTemp = [];
 let dailyPerc = [];
+let home = document.querySelectorAll(".home");
+let main = document.getElementById('main');
 
-document.addEventListener('load', weatherGetter()); // when the page loads, run weatherGetter.
+// Getting the user geolocation.
+if('geolocation' in navigator) { 
+    console.log('geolocation is available');
+    navigator.geolocation.getCurrentPosition((position) => {
+        
+        appendLoader();
+        
+        let lat = position.coords.latitude;
+        let long = position.coords.longitude;
+        
+        // Personal API Key for OpenWeatherMap API
+        let weatherUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${long}&exclude=minutely,alerts&units=imperial&appid=${key}`;
+        
+        // Calling function, catching errors.
+        getWeather(weatherUrl).catch(error => {
+            console.log('error!');
+            console.error(error);
+        });
+        
+        //Creating Charts AFTER data has been created
+        let h = new Date().getHours();
+        charting(ctxHourlyTemp, hourlyTemp, "TEMPERATURE", hourArray(h));
+        charting(ctxHourlyPerc, hourlyPerc, "PERCIPITATION", hourArray(h));
 
-/* Function called by event listener */
-function weatherGetter(){
-    // Getting the user geolocation.
-    if('geolocation' in navigator) { 
-        console.log('geolocation is available');
-        navigator.geolocation.getCurrentPosition((position) => {
-            let lat = position.coords.latitude;
-            let long = position.coords.longitude;
-
-            // Personal API Key for OpenWeatherMap API
-            let weatherUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${long}&exclude=minutely,alerts&units=imperial&appid=${key}`;
-            let [day, month, date, year, time] = (Date()).split(" ");
-            let [hour, minute, second] = (time).split(":");
-            let date1 = `${month} ${date} ${year}`;
-            let time1 = `${hour}:${minute}`;
-            
-            // Calling function, catching errors.
-            getWeather(date1, time1, weatherUrl).catch(error => {
-                console.log('error!');
-                console.error(error);
-            });
-
-            
-            //Creating Charts AFTER data has been created
-            let d = new Date();
-            let w = d.getDay();
-            let h = d.getHours();
-            charting(ctxHourlyTemp, hourlyTemp, "TEMPERATURE", hourArray(h));
-            charting(ctxHourlyPerc, hourlyPerc, "PERCIPITATION", hourArray(h));
-            charting(ctxDailyTemp, dailyTemp, "TEMPERATURE", dayArray(w));
-            charting(ctxDailyPerc, dailyPerc, "PERCIPITATION", dayArray(w));
-        })
-    } else {
-        console.log('geolocation is not avaliable');
-    }
-};
+        let w = new Date().getDay();
+        charting(ctxDailyTemp, dailyTemp, "TEMPERATURE", dayArray(w));
+        charting(ctxDailyPerc, dailyPerc, "PERCIPITATION", dayArray(w));
+    })
+} else {
+    console.log('geolocation is not avaliable');
+}
 
 /* Function to GET Web API Data*/
-async function getWeather(date, time, weatherUrl) {
+async function getWeather(weatherUrl) {
     const responseWeather = await fetch(weatherUrl);
-    data1 = await responseWeather.json();
-    let dailyForcast = data1.current.weather[0].description;
-    let percipitation = `${Math.round(data1.hourly[0].pop*100)}%`;
+    let data1 = await responseWeather.json();
     let temperature = Math.round(data1.current.temp);
     let temperatureF = `${temperature}°F`;
     weatherIcon(data1);
     document.getElementById("temp").textContent = temperatureF;
     chartData(data1);
+    removeLoader();
+    home.forEach((item) => {
+        fadeIn(item);
+    });
 };
 
     // Making the picture match the weather object
@@ -83,20 +79,20 @@ let appendIcon = (pic, phrase) => {
     document.getElementById("advice").textContent = phrase;
 };
 
-let weatherIcon = (data1) => {
-if (data1.current.weather[0].main == 'Clouds'){
-    if (data1.current.weather[0].description == 'few clouds' || data1.current.weather[0].description == 'scattered clouds'){
+let weatherIcon = (weatherObject) => {
+if (weatherObject.current.weather[0].main == 'Clouds'){
+    if (weatherObject.current.weather[0].description == 'few clouds' || weatherObject.current.weather[0].description == 'scattered clouds'){
         appendIcon("./pics/cloudy.svg", "No need for Shades")
     }else{
         appendIcon("./pics/overcast.svg", "It's a bit Dreary")
     }
-}else if (data1.current.weather[0].main == 'Clear'){
+}else if (weatherObject.current.weather[0].main == 'Clear'){
     appendIcon("./pics/sunny.svg", "Bring some Shades")
-}else if (data1.current.weather[0].main == 'Snow'){
+}else if (weatherObject.current.weather[0].main == 'Snow'){
     appendIcon("./pics/snowing.svg", "Wear a Coat")
-}else if (data1.current.weather[0].main == 'Thunderstorm'){
+}else if (weatherObject.current.weather[0].main == 'Thunderstorm'){
     appendIcon("./pics/storming.svg", "Stay Inside Today")
-}else if (data1.current.weather[0].main == 'Drizzle' || data1.current.weather[0].main == 'Rain') {
+}else if (weatherObject.current.weather[0].main == 'Drizzle' || weatherObject.current.weather[0].main == 'Rain') {
     appendIcon("./pics/raining.svg", "Bring an Umbrella")
 } else {
     appendIcon("./index/pics/foggy.svg", "Visibility Low Be Careful")
@@ -132,34 +128,50 @@ chartData = (data) => {
         dailyTemp.push(item.temp.day);
         dailyPerc.push(item.pop*100);
     });
-    data.hourly.forEach((item, index) => {
+    data.hourly.forEach((item) => {
         hourlyTemp.push(item.temp);
         hourlyPerc.push(item.pop*100);
     })
 };
 
-// Making things disappear on click
-let mainChild = document.querySelectorAll(".main-child");
-let main = document.querySelector("#main");
+// checks if tab => compares tab text to class names => fades in/out
 document.addEventListener('click', function(ev){
-    if (ev.target.classList.contains("tab")){
-        mainChild.forEach((item,index)=>{
-            item.classList.toggle('hidden');
-        });
-        for(let i=0; i < main.children.length; i++) {
-            main.children[i].style.cssText="z-index:1;";
-        }
-        if(ev.target.textContent == 'hourly') {
-            document.querySelector('.hourly').classList.toggle('hidden');
-            document.querySelector('.hourly').style.cssText = "z-index: 99";
-        }else if(ev.target.textContent == 'daily') {
-            document.querySelector('.daily').classList.toggle('hidden');
-            document.querySelector('.daily').style.cssText = "z-index: 99";
-        }else if(ev.target.textContent == 'journal') {
-            document.querySelector('.journal').classList.toggle('hidden');
-            document.querySelector('.journal').style.cssText = "z-index: 99";
-        }else {
-            console.log('tab error')
-        };
+    if (ev.target.classList.contains('tab')) {
+        Array.from(main.children).forEach((item) => {
+            if (item.classList.contains(ev.target.innerText)) {
+                fadeIn(item);
+            }else {
+                fadeOut(item);
+            }
+        })
     }
 });
+
+const fadeOut = (endGone) => {
+    endGone.style.opacity = '0';
+    setTimeout(() => {
+        endGone.style.display = 'none';
+    } , 500);
+}
+
+const fadeIn = (endHere) => {
+    endHere.style.display = 'block';
+    setTimeout(() => {
+        endHere.style.opacity = '1';
+    } , 500);
+}
+
+// appends loader after user agrees to submit geolocation
+let appendLoader = () => {
+    let loading = document.createElement('img');
+    loading.src = '/public/pics/loading.svg';
+    loading.classList.add('loader');
+    main.append(loading);
+}
+
+// removes loading icon, once data is fetched
+let removeLoader = () => {
+    Array.from(main.children).forEach((item) => {
+        if(item.classList.contains('loader')) { item.remove() };
+    })
+}
